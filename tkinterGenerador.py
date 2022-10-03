bca5cf07461ca14e0f7259f18d223411ebea9998
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-10-02 21:35:55 -0300

    Update tkinterGenerador.py
    Corrección en el código

--- tkinterGenerador.py
+++ tkinterGenerador.py
@@ -25,17 +25,17 @@ numero2.place(x=130,y=130)
 var1=StringVar()
 var2=StringVar()
 
-spin1=Spinbox(ventana,from_=0,to=100,width=10,textvariable=var1)
+spin1=Spinbox(ventana,from_=0,to=100,width=10,textvariable=var1,state="readonly")
 spin1.place(x=300,y=70)
 
-spin2=Spinbox(ventana,from_=0,to=100,width=10,textvariable=var2)
+spin2=Spinbox(ventana,from_=0,to=100,width=10,textvariable=var2,state="readonly")
 spin2.place(x=300,y=130)
 
 nrGen=Label(ventana,text=" Numero Generado ",font="arial,14",bg="grey", width=15)
 nrGen.place(x=130,y=200)
 
 resultado=StringVar()
-n_generado=Label(ventana,textvariable=resultado,width=10,bg="white")
+n_generado=Label(ventana,textvariable=resultado,width=10,bg="white",state="disabled")
 n_generado.place(x=300,y=200)
 
 generar=Button(ventana,text="Generar",width=10,command=Generar)
